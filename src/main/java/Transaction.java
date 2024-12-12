97575d76887c0cc869c5f6db7299c942e8dd1138
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-12-12 15:03:04 +0200

    Update Transaction.java

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Transaction {
+package org.example;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Transaction {
+    private String date;
+    private double amount;
+    private String description;
 }
